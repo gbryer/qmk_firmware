dc7ccc3d53360b95f5ae0d9224cbb8eba7cb3766
glen <>
2022-05-25 17:11:57 +1000

    Initial keymap

--- keyboards/dz60/config.h
+++ keyboards/dz60/config.h
@@ -56,3 +56,9 @@
 
 /* VIA related config */
 #define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
+
+/* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TAP_TOGGLE 2
+/* Time (in ms) before the one shot key is released */
+#define ONESHOT_TIMEOUT 1000
+
